c65a047e763ed749460c148d99c316ccb1414f6b
lucio <luciomessias2019@gmail.com>
2026-02-19 15:05:52 -0300

    Arquivo de configuração de aplicação atualizado

--- app/ui/src/app/app.config.ts
+++ app/ui/src/app/app.config.ts
@@ -1,14 +1,16 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideZard } from '@/shared/core/provider/providezard';
+import { provideMarkdown } from 'ngx-markdown';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
+    provideMarkdown(),
+    provideZard(),],
     
-  
-    provideZard(),]
 };
